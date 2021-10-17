d2225c1c542150f248978d251ef0f3fe14dc2b55
pure-adachi <takuya_adachi@pure-system.com>
2021-10-17 22:53:07 +0900

    add getTodos

--- cmd/main.go
+++ cmd/main.go
@@ -12,10 +12,26 @@ func main() {
 
 	r := gin.Default()
 	r.GET("/api/sample", getGinSample)
+	r.GET("/api/todos", getTodos)
 
 	r.Run(":" + port)
 }
 
 func getGinSample(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H { "message": "Hello Gin World!" })
+}
+
+func getTodos(c *gin.Context) {
+	type Todo struct {
+		Id int
+		Title string
+	}
+
+	todos := [...] Todo {
+		{ Id: 1, Title: "TODO 1" },
+		{ Id: 2, Title: "TODO 2" },
+		{ Id: 3, Title: "TODO 3" },
+	}
+
+	c.JSON(http.StatusOK, gin.H { "todos": todos })
 }
